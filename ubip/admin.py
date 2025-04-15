79d06904aa08172268d8ac57ed62e6b182f9d425
hj.shakur <h3ctorcervantes11@gmail.com>
2025-04-15 14:15:37 -0700

    migrations and model creation

--- ubip/admin.py
+++ ubip/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from . models import Direction_ip, zonas_geograficas
 
 # Register your models here.
+
+admin.site.register(Direction_ip)
+admin.site.register(zonas_geograficas)
--- ubip/models.py
+++ ubip/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Direction_ip(models.Model):
+    ip_inicio = models.CharField(max_length=20)
+    ip_final = models.CharField(max_length=20)
+    asn = models.CharField(max_length=100)  
+    provedor = models.CharField(max_length=100)
+    id_zona_georafica = models.ForeignKey(
+        'zonas_geograficas',  
+        on_delete=models.CASCADE      )
+
+    class Meta:
+        verbose_name = 'Rango de IP'
+        db_table = 'direction_ip'
+
+class zonas_geograficas(models.Model):
+    estado_o_provincia = models.CharField(max_length=100) 
+    ciudad = models.CharField(max_length=100) 
+    coordenadas = models.CharField(max_length=100)
+    nivel_de_conectividad = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = 'Zona Geografica'
+        db_table = 'zonas_geograficas'
